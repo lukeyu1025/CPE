22a08b18816bd404cf5d778b291a32d9506bf9b7
lukeyu1025 <74660025+lukeyu1025@users.noreply.github.com>
2022-09-16 23:45:16 +0800

    Update UVA10162.cpp

--- UVA10162.cpp
+++ UVA10162.cpp
@@ -2,56 +2,37 @@
 #include <cmath>
 using namespace std;
 int main(){
-	long long num;
-	cin>>num;
-	while(num!=0){
-		int ans=0;
-		for(int i=1;i<=num;i++){
-		int ten=i%10,temp=i;
-		if(ten==1)ans+=1;
-		if(ten==2){
-			temp%=4;
-			if(temp==0)ans+=16;
-			else if(temp==1)ans+=2;
-			else if(temp==2)ans+=4;
-			else if(temp==3)ans+=8;
-		}
-		else if(ten==3){
-			temp%=4;
-			if(temp==0)ans+=1;
-			else if(temp==1)ans+=3;
-			else if(temp==2)ans+=9;
-			else if(temp==3)ans+=7;
-		}
-		else if(ten==4){
-			temp%=2;
-			if(temp==0)ans+=6;
-			else if(temp==1)ans+=4;
-		}
-		else if(ten==5)ans+=5;
-		else if(ten==6)ans+=6;
-		else if(ten==7){
-			temp%=4;
-			if(temp==0)ans+=1;
-			else if(temp==1)ans+=7;
-			else if(temp==2)ans+=9;
-			else if(temp==3)ans+=3;
-		}
-		else if(ten==8){
-			temp%=4;
-			if(temp==0)ans+=6;
-			else if(temp==1)ans+=8;
-			else if(temp==2)ans+=4;
-			else if(temp==3)ans+=2;
-		}
-		else if(ten==9){
-			temp%=2;
-			if(temp==0)ans+=1;
-			else if(temp==1)ans+=9;
-		}
+string str="";
+	cin>>str;
+	while(str!="0"){
+		int n=0;
+		if(str.length()==1) n=str[0]-48;
+		else n=(int)str[str.length()-1]-48+10*(int)(str[str.length()-2]-48);
+		n%=100;
+		int temp=n/20;
+		int ans=temp*4;
+		n%=20;
+		if(n==1)ans+=1;
+		else if(n==2)ans+=5;
+		else if(n==3)ans+=2;
+		else if(n==4)ans+=8;
+		else if(n==5)ans+=3;
+		else if(n==6)ans+=9;
+		else if(n==7)ans+=2;
+		else if(n==8)ans+=8;
+		else if(n==9)ans+=7;
+		else if(n==10)ans+=7;
+		else if(n==11)ans+=8;
+		else if(n==12)ans+=4;
+		else if(n==13)ans+=7;
+		else if(n==14)ans+=3;
+		else if(n==15)ans+=8;
+		else if(n==16)ans+=4;
+		else if(n==17)ans+=1;
+		else if(n==18)ans+=5;
+		else if(n==19)ans+=4;
 		ans%=10;
-		}
 		cout<<ans<<endl;
-		cin>>num;
+		cin>>str;
 	}
 }
