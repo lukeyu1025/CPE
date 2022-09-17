5a6417744d1b465a4cbf6e60243793463973fcbc
lukeyu1025 <74660025+lukeyu1025@users.noreply.github.com>
2022-09-17 13:17:50 +0800

    Update UVA10215.cpp

--- UVA10215.cpp
+++ UVA10215.cpp
@@ -1,26 +1,22 @@
-//未完成
 #include <iostream>
 using namespace std;
 int main(){
-	float L,W;
+	double L,W;
 	while(cin>>L>>W){
-		float max=0,maxv,minv,min=100,volume;
+		double max=0,maxv,minv,min=100,volume;
 		if(L>W){//
-			float temper=W;
+			double temper=W;
 			W=L;
 			L=temper;
 		}
-		for(float i=0.00001;2*i<L;i+=0.00001){
+		for(double i=0.00001;2*i<=L;i+=0.00001){
 			volume=i*(L-(2*i))*(W-(2*i));
-			if(volume<min){
-			minv=i;
-			min=volume;
-			}
 			if(volume>max){
 				maxv=i;
 				max=volume;
 			}
 		}
+		minv=L/2;
 		printf("%1.3f 0.000 %1.3f\n",maxv,minv);
 	}
 }
