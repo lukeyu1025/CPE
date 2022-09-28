938be486af3add3597a71b1b8d220c3842d18a8b
lukeyu1025 <74660025+lukeyu1025@users.noreply.github.com>
2022-09-28 17:19:38 +0800

    Update UVA10222.cpp

--- UVA10222.cpp
+++ UVA10222.cpp
@@ -1,50 +1,39 @@
 #include <iostream>
-#include <cctype>
 using namespace std;
 int main(){
-	int n;
-	cin>>n;
-	string str="";
-	getline(cin,str);
-	for(int i=0;i<n;i++){
-		getline(cin,str);
-		for(int q=0;q<=str.length();q++){
-			char ch=tolower(str[q]);
-			if(ch=='q')str[q]='-';
-			if(ch=='a')str[q]=']';
-			if(ch=='z')str[q]=';';
-			if(ch=='w')str[q]='=';
-			if(ch=='s')str[q]='\\';
-			if(ch=='x')str[q]='\'';
-			if(ch=='e')str[q]='q';
-			if(ch=='d')str[q]='a';
-			if(ch=='c')str[q]='z';
-			if(ch=='r')str[q]='w';
-			if(ch=='f')str[q]='s';
-			if(ch=='v')str[q]='x';
-			if(ch=='t')str[q]='e';
-			if(ch=='g')str[q]='d';
-			if(ch=='b')str[q]='c';
-			if(ch=='y')str[q]='r';
-			if(ch=='h')str[q]='f';
-			if(ch=='n')str[q]='v';
-			if(ch=='u')str[q]='t';
-			if(ch=='j')str[q]='g';
-			if(ch=='m')str[q]='b';
-			if(ch=='I')str[q]='y';
-			if(ch=='i')str[q]='y';
-			if(ch=='k')str[q]='h';
-			if(ch==',')str[q]='n';
-			if(ch=='o')str[q]='u';
-			if(ch=='l')str[q]='j';
-			if(ch=='.')str[q]='m';
-			if(ch=='p')str[q]='i';
-			if(ch==';')str[q]='k';
-			if(ch=='/')str[q]=',';
-			if(ch=='[')str[q]='o';
-			if(ch=='\'')str[q]='l';
-			if(ch==']')str[q]='p';
+	string str;
+	while(getline(cin,str)){
+		for(int i=0;i<str.length();i++){
+			str[i]=tolower(str[i]);
+			if(str[i]=='e')str[i]='q';
+			if(str[i]=='d')str[i]='a';
+			if(str[i]=='c')str[i]='z';
+			if(str[i]=='r')str[i]='w';
+			if(str[i]=='f')str[i]='s';
+			if(str[i]=='v')str[i]='x';
+			if(str[i]=='t')str[i]='e';
+			if(str[i]=='g')str[i]='d';
+			if(str[i]=='b')str[i]='c';
+			if(str[i]=='y')str[i]='r';
+			if(str[i]=='h')str[i]='f';
+			if(str[i]=='n')str[i]='v';
+			if(str[i]=='u')str[i]='t';
+			if(str[i]=='j')str[i]='g';
+			if(str[i]=='m')str[i]='b';
+			if(str[i]=='i')str[i]='y';
+			if(str[i]=='k')str[i]='h';
+			if(str[i]==',')str[i]='n';
+			if(str[i]=='o')str[i]='u';
+			if(str[i]=='l')str[i]='j';
+			if(str[i]=='.')str[i]='m';
+			if(str[i]=='p')str[i]='i';
+			if(str[i]==';')str[i]='k';
+			if(str[i]=='/')str[i]=',';
+			if(str[i]=='[')str[i]='o';
+			if(str[i]=='\'')str[i]='l';
+			if(str[i]==']')str[i]='p';
+			cout<<str[i];
 		}
-		cout<<str<<endl;
+		cout<<endl;
 	}
 }
