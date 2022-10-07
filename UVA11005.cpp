350e082c2657eb9f1f944b7657d523f8a39732b5
lukeyu1025 <74660025+lukeyu1025@users.noreply.github.com>
2022-10-07 22:32:51 +0800

    Update UVA11005.cpp

--- UVA11005.cpp
+++ UVA11005.cpp
@@ -1,4 +1,3 @@
-//未完成
 #include <iostream>
 using namespace std;
 int main(){
@@ -14,9 +13,28 @@ int main(){
 		int n;
 		cin>>n;
 		for(int j=0;j<n;j++){
-			int temp;
-			cin>>temp;
-			cout<<"Cheapest base(s) for number "<<temp<<": "<<endl;
+			int input,temp[35],tmp;
+			cin>>input;
+			for(int q=0;q<35;q++)temp[q]=0;
+			for(int q=2;q<=36;q++){
+				tmp=input;
+				while(tmp!=0){
+					temp[q-2]+=arr[tmp%q];
+					tmp/=q;
+				}
+			}
+			int minnum=10000;
+			for(int q=0;q<35;q++){
+				if(minnum>temp[q])minnum=temp[q];
+			}
+			cout<<"Cheapest base(s) for number "<<input<<":";
+			for(int q=0;q<35;q++){
+				if(minnum==temp[q]){
+					cout<<" "<<q+2;
+				}
+			}
+			cout<<endl;
 		}
+		if(i!=t-1)cout<<endl;
 	}
 }
